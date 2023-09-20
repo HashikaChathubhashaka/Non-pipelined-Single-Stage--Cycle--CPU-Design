89043814fb4e711678485775d50d7145d2342f9f
Hashika Chathubhashaka <hashikachathubhashaka@gmail.com>
2023-09-21 01:43:06 +0530

    alu

--- design files/ALU.sv
+++ design files/ALU.sv
@@ -1,15 +1,18 @@
 module ALU(
     input logic [31:0] a,
     input logic [31:0] b,
-    input logic [2:1] ALU_control,
-    output logic carry,overflow,zero,negative,
+
+    input logic [2:0] ALU_control,
+    output logic zero,overflow,negative,
     output logic [31:0] ALU_result
 
 );
 
+
 logic cout;
 logic [31:0] sum;
 
-    
-    
+
+
 endmodule
+
